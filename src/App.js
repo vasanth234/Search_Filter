1fa772ea75cfc7882dc268ac780afdf36d6e40c3
Vasanth-Pippari <pipparivasanth66700@gmail.com>
2024-05-23 20:55:12 +0530

    filter

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import React from 'react'
+import {useState} from 'react'
+import Input from './City.json'
+import './App.css'
+const App = () => {
+  const [data,setData]=useState('')
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <div>
+      <center>
+        <h2>Enter the city</h2>
+        <br/>
+        <div className='fty'>
+        <input type='text' placeholder='city' value={data} onChange={(e)=>setData(e.target.value)}/>
+        </div>
+        <br/>
+        {Input.filter(city=>city.city.toLowerCase().includes(data)).map((city)=>{
+         return( <div style={{"border":"1px solid black","padding":"10px","margin":"10px","maxWidth":"70%"}}>
+             {city.city}
+            
+          </div>
+         )
+        })}
+      </center>
+     
     </div>
-  );
+    </>
+  )
 }
 
-export default App;
+export default App
